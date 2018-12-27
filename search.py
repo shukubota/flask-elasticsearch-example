2710625ff312534b9d14aa583fb83f6c58ad308f
shukubota <kubotashu0421@gmail.com>
2018-12-27 22:56:36 +0900

    searchして見る

--- search.py
+++ search.py
@@ -1,29 +1,8 @@
-import os
 from elasticsearch import Elasticsearch
 
-dirname = "gutenberg"
-
 es = Elasticsearch()
+res = es.search(index='gutenberg_books', body={'_source': ['title', 'author'], 'query': {'match': {'text':'Bennet'}}})
 
-print(os.listdir(dirname))
-print(enumerate(os.listdir(dirname)))
-
-for id, fname in enumerate(os.listdir(dirname)):
-  print(id)
-  print(fname)
-  text, title, author = '', '', ''
-
-  with open(os.path.join(dirname, fname), 'rt') as f:
-    header = True
-    for line in f:
-      if not header:
-        text += line
-      elif line.startswith('*** START OF THIS PROJEC'):
-        header = False
-      elif line.startswith('Title:'):
-        title = line.split(':')[1].strip()
-      elif line.startswith('Author:'):
-        author = line.split(':')[1].strip()
-
-  doc = { 'text': text, 'title': title, 'author': author, 'file_name': fname }
-  es.index(index='gutenberg_books', doc_type='gutenberg', id=id+1, body=doc)
+#print(res['hits']['hits'].count)
+for doc in res['hits']['hits']:
+  print(doc['_source'])
